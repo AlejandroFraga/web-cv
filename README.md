43ef55e95f4f55c23729222e7934c0ff9416b9c0
Alejandro Fraga Cimadevila <alex_fraga95@hotmail.com>
2020-10-23 02:24:24 +0200

    website v1.0
    Upload of the website, with projects and about sections implemented. Compatibility with all devices.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# web-cv
-My personal web page 
+# WEB CV
+This is the project for my personal web page in which I talk about my profesional career
+
+### TO-DO
+* End cleaning and commenting all the HTML, CSS and Javascript code
+* I want to make a new section that will correspond to a videogame in HTML*
+* HTTPS instead of HTTP to avoid insecure connection
+
+*In this section you'll can play as me in an interactive CV. So instead of reading boring details, you'll be reading boring details inside a videogame. Who doesn't love that?
+
+### Sections
+
+The web page is divided in these sections:
+
+  - [Projects](http://alejandrofraga.me) - Here I show all my projects that are at least WIP
+  - [About](http://alejandrofraga.me/about.html) - Here I talk a little about my studies and experience
+
+### Used tools
+
+These are the tools that I used for the development of the web page.
+| Tool | Info |
+| ------ | ------ |
+| [Materialize](https://materializecss.com) | Materialize, a CSS Framework based on material design
+| [jQuery](https://jquery.com) | New Wave JavaScript |
---
+++ html/resources/js/init.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    /* SIDENAV LOAD */
+    $('.sidenav').sidenav();
+    
+    /* MAKE THE FIRST CHECK BEFORE EVEN SCROLL */
+    checkAppearAnimations();
+    
+    /* VISIBLE IN SCREEN ANIMATIONS */
+    $(document).scroll(function(evt) {
+        checkAppearAnimations();
+    });
+});
+
+/* WE CHECK IF SOMETHING THAT HAS TO APPEAR IS ON THE SCREEN */
+function checkAppearAnimations() {
+    var v1 = $(this).scrollTop();
+    var i = 0;
+    
+    /* FOR EACH '.APPEAR' ELEMENT WE CALCULATE IF IT'S BEEN SHOWN ON SCREEN */
+    $('.appear').each(function() {
+        var element = $(this);
+        var v2 = $(this).position().top - $(window).height();
+        var v3 = $(this).position().top + $(this).height();
+        
+        /* IT'S SHOWN, SO WE PREPARE THE ELEMENT TO APPEAR */
+        if( v1 > v2 && v1 < v3) {
+            i++;
+            prepareAppear(element, i);
+        }
+    });
+}
+
+/* WE PREPARE AN ELEMENT TO APPEAR, WITH A DELAY DEFINED BY IT'S POSITION */
+function prepareAppear(element, i) {
+    
+    /* BEFORE APPEAR, WE MAKE IT DISAPPEAR */
+    element.css({opacity: 0.0, visibility: "visible"});
+
+    /* WE SET A TIMEOUT, SO IF A LOT OF ELEMENTS HAVE TO APPEAR AT THE SAME TIME, THEY'RE GRADUALLY SHOWN */
+    setTimeout(function() {
+        element.removeClass('appear');
+        appearAnimation(element);
+    }, 0 + ( i * 100 ));
+}
+
+/* ANIMATE THE ELEMENT TO APPEAR AFTER A DEFINED TIME */
+function appearAnimation(element) {
+    var appearDuration = 1000;
+    
+    element.css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0}, appearDuration);
+}
